99803670ea15d73298ad41e91ecb45f95364a593
ShazamXD <chitransh.pal108@gmail.com>
2025-08-29 03:44:31 +0530

    created own react library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,7 +3,7 @@
 function App() {
   
 
-  return (
+  return ( // has the capacity to return only one tag so div is used and multiple tags are put into that div so that multiple tags can be returned (tricked hahaha)
     <h1>Chai Aur React with Vite</h1>
   )
 }
